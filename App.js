d0cf86dbed27b91a68df451b0dd8636651be49c9
Ryan Felone <ryanfelone@gmail.com>
2018-04-23 22:18:36 -0400

    idk wtf is going wrong

--- App.js
+++ App.js
@@ -1,67 +1,28 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Animated, Marker } from 'react-native-maps';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-export const getCurrentLocation = () => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(position => resolve(position), e => reject(e));
-  });
-};
-
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = 0.0421
 
 export default class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {
-      region: {
-        latitude: 42.3601,
-        longitude: -71.0589,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      },
-      marker: {
-        latitude: 42.3601,
-        longitude: -71.0589
-      }
-    };
-  }
-
-  componentDidMount() {
-
-  }
-
-
-  onRegionChange(region) {
-    this.setState({ region });
-  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          {this.state.marker.latitude}
-        </Text>
-      <MapView style={styles.map}
-        provider={'google'}
-        showsUserLocation={true}
-        initialRegion={this.state.region}
-        onPress={ event => this.setState({marker: event.nativeEvent.coordinate}) }
-        >
-        <MapView.Marker
-        coordinate={ this.state.marker }
-        />
-      </MapView>
+          <MapView.Animated style={styles.map}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
       </View>
     );
   }
 }
 
-function markerClick() {
-  console.log('test?');
-}
 
 
 const styles = StyleSheet.create({
