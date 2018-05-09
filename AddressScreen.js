968bbf42accc260d5452163ea6c04a00dc3b9ed7
Ryan Felone <rfelone@normatecusa.com>
2018-05-09 15:01:19 -0400

    address search working better

--- AddressScreen.js
+++ AddressScreen.js
@@ -8,14 +8,15 @@ export default class AddressScreen extends React.Component {
         <GooglePlacesAutocomplete
           placeholder='Search'
           minLength={2} // minimum length of text to search
-          autoFocus={false}
-          returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-          listViewDisplayed='auto'    // true/false/undefined
+          autoFocus={true}
+          returnKeyType={'default'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
+          listViewDisplayed={true}
           fetchDetails={true}
-          renderDescription={(row) => row.description} // custom description render
+          renderDescription={row => row.description} // custom description render
           onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
             console.log(data);
             console.log(details);
+
           }}
           getDefaultValue={() => {
             return ''; // text input default value
@@ -24,7 +25,6 @@ export default class AddressScreen extends React.Component {
             // available options: https://developers.google.com/places/web-service/autocomplete
             key: 'AIzaSyA9vmylTlBXWBpNeiRdLUKt_75LnjakXMQ',
             language: 'en', // language of the results
-            types: '(cities)' // default: 'geocode'
           }}
           styles={{
             description: {
@@ -38,20 +38,10 @@ export default class AddressScreen extends React.Component {
           currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
           currentLocationLabel="Current location"
           nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-          GoogleReverseGeocodingQuery={{
-            // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-          }}
-          GooglePlacesSearchQuery={{
-            // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-            rankby: 'distance',
-            types: 'food'
-          }}
 
-          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-          //predefinedPlaces={[homePlace, workPlace]}
 
           debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-          renderRightButton={() => <Text>Custom text after the inputg</Text>}
+          renderRightButton={() => <Text>Go Here!</Text>}
         />
       );
     }
